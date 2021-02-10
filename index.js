b7c9addc9105da63f08f84ad5cda6a4d92e39654
Desiree Lerma <desi.lerma96@yahoo.com>
2021-02-09 22:22:02 -0600

    commit tree 6ef2311286ace3f26c8ee9eec30eb3ef1e7f0041

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your functions here
+function map(sourceArray, funct) {
+    // include repeated code
+    let newArray = []
+    for(let i = 0; i < sourceArray.length; i++) {
+        // make a call to function, input sourceArray element as arg
+        newArray.push(funct(sourceArray[i]))
+    }
+    return newArray
+}
+
+function reduce(sourceArray, funct, start) {
+    // copy as much repeated code
+    let total
+    let i 
+    if (!!start) {
+        // if a start num is present
+        total = start
+        i = 0
+    }
+    else {
+        // if start is not present
+        total = sourceArray[0]
+        i = 1
+    }
+    for (i; i < sourceArray.length; i++) {
+        total = funct(sourceArray[i], total)
+    }
+    return total
+}
